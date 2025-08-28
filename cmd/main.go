11d3025ec5a2d62bbc6ecb9c289478df9c285d74
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-08-27 22:20:59 -0300

    feat:add base 64 auth

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -22,6 +24,28 @@ type Body struct {
 }
 
 func router(ctx context.Context, event events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+
+	authorization := event.Headers["Authorization"]
+
+	payload := strings.TrimPrefix(authorization, "Basic ")
+
+	decoded, err := base64.StdEncoding.DecodeString(payload)
+
+	if err != nil {
+		return events.LambdaFunctionURLResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       http.StatusText(http.StatusInternalServerError),
+		}, nil
+	}
+
+	authParts := strings.SplitN(string(decoded), ":", 2)
+
+	if authParts[0] != "edu" || authParts[1] != "edu" {
+		return events.LambdaFunctionURLResponse{
+			StatusCode: http.StatusUnauthorized,
+			Body:       http.StatusText(http.StatusUnauthorized),
+		}, nil
+	}
 
 	var b Body
 
